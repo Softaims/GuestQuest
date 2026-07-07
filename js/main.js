698b9735fa14a73d29a52579c97f516a52bfa3fb
Hello <adilsarfr00@gmail.com>
2026-07-07 20:59:57 +0500

    create hero section and Use navbar nad create footer nad add all sections sturcture

--- js/main.js
+++ js/main.js
@@ -9,3 +9,65 @@ $(function () {
     });
 
 });
+
+
+$(function () {
+
+    $(".chip").on("click", function () {
+
+        $(this)
+            .siblings()
+            .removeClass("active");
+
+        $(this)
+            .addClass("active");
+
+    });
+
+    $("#clearFilters").click(function (e) {
+
+        e.preventDefault();
+
+        $(".chip").removeClass("active");
+
+    });
+
+});
+
+
+
+// grid section 
+
+$(function () {
+
+    $(".view-btn").click(function () {
+
+        $(".view-btn").removeClass("active");
+
+        $(this).addClass("active");
+
+        let view = $(this).data("view");
+
+        if (view === "grid") {
+
+            $("#resultsContainer")
+
+                .removeClass("results-list")
+
+                .addClass("results-grid");
+
+        }
+
+        else {
+
+            $("#resultsContainer")
+
+                .removeClass("results-grid")
+
+                .addClass("results-list");
+
+        }
+
+    });
+
+});
